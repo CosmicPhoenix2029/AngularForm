b81f0423e37e0a1ddf5fce54e2994683af7425e9
CosmicPhoenix2029 <haydnatkinson6@gmail.com>
2023-07-19 17:28:44 +0100

    created the ticket data model

---
+++ template-driven-form/src/ticket.ts
@@ -0,0 +1,22 @@
+export class ticket {
+    constructor(
+        contact: string,
+        contactEmail: string,
+        organisation: string,
+        room: string,
+        building: string,
+        phone: string,
+        mobile: string,
+        contactPreference: string,
+        subject: string,
+        description: string,
+        techNotes: string,
+        files: File,
+        notRequiredBefore: Date,
+        siteVisitRequired: boolean,
+        assetNumber: string,
+        catagory: string,
+        team: string,
+        minsContributed: number
+    ) { }
+}
